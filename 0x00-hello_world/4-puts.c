79ea323d47412a520b4fb8e379aa0aae3b9268fc
Seife1 <gebrealabate19@yahoo.com>
2023-03-16 16:23:44 -0700

    low level programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+/*
+ * Print all the content with puts
+ * End with a value 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
