9551b493300d3503ca857eb931bee1e43d2a6111
Seife1 <gebrealabate19@yahoo.com>
2023-03-16 18:00:59 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle,
  * followed by a newline."
- * return 0.
+ * return:0 (Success).
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
