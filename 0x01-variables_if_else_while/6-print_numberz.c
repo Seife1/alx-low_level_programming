a4a233f27efcbe7ba230651cf744831c516baa8b
Seife1 <gebrealabate19@yahoo.com>
2023-03-19 05:37:16 -0700

    Number as a char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	/*Print numbers*/
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
