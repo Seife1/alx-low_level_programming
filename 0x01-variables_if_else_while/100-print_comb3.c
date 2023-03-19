1f7fc10fc8a4edda062e1e80047eb09dc7fca68d
Seife1 <gebrealabate19@yahoo.com>
2023-03-19 07:38:58 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, j;
+	/*Two digit nembers(ij) ---> i < j*/
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9' ; j++)
+		{
+		if (i < j)
+		{
+		putchar(i);
+		putchar(j);
+		if (i == '8' && j == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
