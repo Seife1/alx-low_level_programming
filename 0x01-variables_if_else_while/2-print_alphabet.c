faf11a4ea9a3f6b1727d74b863d81060586242b0
Seife1 <gebrealabate19@yahoo.com>
2023-03-19 02:16:11 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	/* print lowercase alphabet*/
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
