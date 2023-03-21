177e5bbe78ee10004548300b323e246fba620185
Seife1 <gebrealabate19@yahoo.com>
2023-03-21 14:57:30 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	/*Nested loop*/
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	return ;
+}
