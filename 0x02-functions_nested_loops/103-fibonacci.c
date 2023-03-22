bfe352cefc4a1c22bc9a87d77945de4a5e08e23b
Seife1 <gebrealabate19@yahoo.com>
2023-03-22 13:45:33 -0700

    Even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int sum, k = 1, j, i = 0;
+
+	for (j = 1; j <= 50; j++)
+	{
+		sum = i + k;
+		i = k;
+		k = sum;
+		if ((unsigned long)sum <= 4000000 && (unsigned long)sum % 2 == 0)
+		{
+			printf("%lu, ", (unsigned long)sum);
+		}
+	}
+	printf("\n");
+	return (0);
+}
