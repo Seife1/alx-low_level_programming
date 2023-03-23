6286856a1d04e93c99bd2ee997416e3afbf7e6ca
Seife1 <gebrealabate19@yahoo.com>
2023-03-23 16:18:20 -0700

    MULTIPLICATION

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - find mul of two numbers
+ * @a: first number
+ * @b: Second number
+ * Return: Always 0.
+ *
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
