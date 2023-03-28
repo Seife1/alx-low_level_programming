3e68414bf39793f468ef767add134855740b89fc
Seife1 <gebrealabate19@yahoo.com>
2023-03-28 05:44:56 -0700

    return all the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
