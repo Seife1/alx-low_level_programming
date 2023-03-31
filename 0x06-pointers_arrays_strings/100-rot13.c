15f84a2aa194dcfc51a52139a12fe87df7b2c6b3
Seife1 <gebrealabate19@yahoo.com>
2023-03-31 11:28:53 -0700

    root13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode into rot13
+ * @n: input
+ * Return: n value
+ */
+
+char *rot13(char *n)
+{
+	int i, j;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
