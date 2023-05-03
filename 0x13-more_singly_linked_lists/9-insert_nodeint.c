d3850c90e55308e720ae885fce5e0cc8760562c2
Seife1 <gebrealabate19@yahoo.com>
2023-05-02 19:35:33 -0700

    Insert node @.. Updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
 	if (idx != 0)
 	{
 		tmp = *head;
@@ -28,18 +33,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
 	new_node->n = n;
+	new_node->next = NULL;
+
 	if (idx == 0)
 	{
-		new_node = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	new_node = tmp->next;
+	new_node->next = tmp->next;
 	tmp->next = new_node;
 	return (new_node);
 }
